7a3a110e57a215b48b8a67c413f7477ea7b6b13d
Gaurav Kuwar <gauravk@Gauravs-MacBook-Pro.local>
2022-05-20 22:30:31 -0400

    sql file changes

--- course_schedule_sql_file.sql
+++ course_schedule_sql_file.sql
@@ -9,26 +9,37 @@ CREATE TABLE Room (
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE Class (
-	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(50),
-	year YEAR,
-	term VARCHAR(50),
-	start_time TIME,
-	end_time TIME,
-	room_id INT NOT NULL, -- maybe could be NULL since online classes idk
-	professor_id INT NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (room_id) REFERENCES Room(id),
-	FOREIGN KEY (professor_id) REFERENCES Professor(id)
-);
-
 CREATE TABLE Professor (
 	id INT NOT NULL AUTO_INCREMENT,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
 	PRIMARY KEY (id)
 );
+
+CREATE TABLE Class (
+	id INT NOT NULL AUTO_INCREMENT,
+	year YEAR,
+	term VARCHAR(50), -- only SPRING, FALL, SUMMER and WINTER
+	course_id INT,
+	start_time TIME, -- constraints for diff class in same room at same time
+	end_time TIME,
+	room_id INT NOT NULL, -- maybe could be NULL since online classes idk
+	professor_id INT NOT NULL,
+	PRIMARY KEY (id, year, term),
+	FOREIGN KEY (room_id) REFERENCES Room(id),
+	FOREIGN KEY (professor_id) REFERENCES Professor(id),
+	FOREIGN KEY (course_id) REFERENCES Course(id)
+);
+
+CREATE TABLE Course ( -- 1, SWE/CSC322
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(50)
+);
+
+INSERT INTO Room VALUES
+	(1, "A");
+
+SELECT * FROM Room;
 
 
 
